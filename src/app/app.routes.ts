def4440275f1964fe0b14b73da25cc8e17fd1d7e
JohnAchicaizaG <159038108+JohnAchicaizaG@users.noreply.github.com>
2025-04-04 15:12:09 -0500

    tailwindcss

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,33 @@
 import { Routes } from '@angular/router';
+import { AuthLayoutComponent } from '@core/layout/auth-layout/auth-layout.component';
+import { DashboardLayoutComponent } from '@features/dashboard/layout/dashboard-layout/dashboard-layout.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+	{
+		path: '',
+		component: AuthLayoutComponent,
+		children: [
+			{
+				path: 'login',
+				loadComponent: () =>
+					import('@features/auth/pages/login/login-page.component'),
+			},
+			{
+				path: 'register',
+				loadComponent: () =>
+					import('@features/auth/pages/register/register-page.component'),
+			},
+		],
+	},
+	{
+		path: 'dashboard',
+		component: DashboardLayoutComponent,
+		children: [
+			{
+				path: '',
+				loadComponent: () =>
+					import('@features/dashboard/pages/home/home.component'),
+			},
+		],
+	},
+];
