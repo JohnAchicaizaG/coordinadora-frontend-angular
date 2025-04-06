65740d3caf767c5f0b82a55b0f24dbfb0f7a6af8
JohnAchicaizaG <159038108+JohnAchicaizaG@users.noreply.github.com>
2025-04-06 11:18:15 -0500

    Implementación de formularios de inicio de sesión y registro con validación, mejora en el diseño responsivo y redirección a la página de inicio de sesión. Actualización de estilos CSS y estructura de componentes.

--- src/app/features/auth/pages/register/register-page.component.ts
+++ src/app/features/auth/pages/register/register-page.component.ts
@@ -1,8 +1,8 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
-
+import { RegisterFormComponent } from '@features/auth/components/register/register-form.component';
 @Component({
 	selector: 'app-register-page',
-	imports: [],
+	imports: [RegisterFormComponent],
 	templateUrl: './register-page.component.html',
 	changeDetection: ChangeDetectionStrategy.OnPush,
 })
